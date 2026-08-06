ca5634548b45326149dc49a1f330ee6b62a8b104
Omar-Maher-Khatib <s12144231@stu.najah.edu>
2026-08-06 13:11:22 +0300

    adjust home page mode button

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const toggleTheme = document.querySelector("#theme-toggle");
+const themeIcon = document.querySelector("#themeIcon");
 const html = document.documentElement;
 
 function applyTheme(theme) {
     if (theme === "dark") {
         html.classList.add("dark");
+        themeIcon.innerHTML = `<img src="./assets/img/header/light.svg" alt="Sun" class="w-6 h-6 object-contain" />`
     } else {
         html.classList.remove("dark");
+        themeIcon.innerHTML = `<img src="./assets/img/header/dark.svg" alt="Moon" class="w-6 h-6 object-contain" />`
     }
 }
 
